e6ae889afb998fd17d72c8aec4472f004a51e3a1
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2020-09-24 20:01:33 -0600

    Convert python dictionaries to prolog dictionaries directly.

--- miner.py
+++ miner.py
@@ -3,20 +3,49 @@ import time
 from iexfinance.stocks import Stock
 
 
+def format_dict(d):
+    last = len(d) - 1
+    s = ''
+    s += '_{'
+
+    for i, (k, v) in enumerate(d.items()):
+        if v is None:
+            s += "{}: '{}'".format(format_kv(k), None)
+        elif type(v) is dict:
+            s += '{}: '.format(format_kv(k))
+            s += format_dict(v)
+        else:
+            s += '{}: {}'.format(format_kv(k), format_kv(v))
+
+        if i < last:
+            s += ', '
+
+    s += '}'
+
+    return s
+
+
+def format_kv(x):
+    if type(x) is str:
+        return "'{}'".format(x)
+
+    return str(x)
+
+
 def main():
-    tickers = []
+    tickers = set([])
 
     with open('crsp_large_cap_value.csv', newline='') as equities_file:
         reader = csv.reader(equities_file)
 
         for row in reader:
-            tickers.append(row[0])
+            tickers.add(row[0])
 
-    with open('stocks.pl', mode='w') as stocks_db:
+    with open('equities.lgt', mode='w') as stocks_db:
         for ticker in tickers:
             stock = Stock(ticker)
-            peers, advanced_stats = stock.get_peers(), stock.get_advanced_stats()
-            stocks_db.write("stock('{}', {}, {}).\n".format(ticker, peers, advanced_stats))
+            peers, advanced_stats = set(stock.get_peers()), stock.get_advanced_stats()
+            stocks_db.write("stock('{}', {}, {}).\n".format(ticker, list(peers), format_dict(advanced_stats)))
             time.sleep(0.5)
 
 
